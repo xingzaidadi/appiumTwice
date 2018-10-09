2a18bfe446f5a059b01d7376238125471f71f287
xingzaidadi <1776200391@qq.com>
2018-10-09 19:01:24 +0800

    master add

--- A.java
+++ A.java
@@ -1,8 +1,9 @@
 public class A{
    
    public static void main(String args[]){
-           //一条注释而已，哈哈哈哈
+           //一条注释而已，哈哈哈哈,hahahaha
 	   System.out.println("asdasdasd");
+	   //master add
    }
 
 }
